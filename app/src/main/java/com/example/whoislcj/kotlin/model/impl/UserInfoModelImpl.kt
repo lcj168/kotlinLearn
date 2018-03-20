d815363d6fcf9c4e00bdab10566daf7e97eaa8c8
whoislcj <102160369@qq.com>
2018-03-20 20:55:27 +0800

    kotlin mvp 完整使用

--- app/src/main/java/com/example/whoislcj/kotlin/model/impl/UserInfoModelImpl.kt
+++ app/src/main/java/com/example/whoislcj/kotlin/model/impl/UserInfoModelImpl.kt
@@ -3,6 +3,7 @@ package com.example.whoislcj.kotlin.model.impl
 import com.example.whoislcj.kotlin.model.bean.UserInfo
 import com.example.whoislcj.kotlin.model.interfaces.GetUserInfoListener
 import com.example.whoislcj.kotlin.model.interfaces.IUserInfoModel
+import java.util.logging.Handler
 
 /**
  * Created by whoislcj on 2018/3/20.
@@ -11,10 +12,12 @@ class UserInfoModelImpl : IUserInfoModel {
 
 
     override fun getUserInfo(listener: GetUserInfoListener) {
-        val userInfo = UserInfo();
-        userInfo.age = 28;
-        userInfo.name = "lcj";
-        listener.onGetUserInfo(userInfo);
+        android.os.Handler().postDelayed({
+            val userInfo = UserInfo();
+            userInfo.age = 28;
+            userInfo.name = "lcj";
+            listener.onGetUserInfo(userInfo);
+        },5000)
     }
 
 
--- app/src/main/java/com/example/whoislcj/kotlin/view/activity/MvpDemoActivity.kt
+++ app/src/main/java/com/example/whoislcj/kotlin/view/activity/MvpDemoActivity.kt
@@ -1,37 +1,48 @@
 package com.example.whoislcj.kotlin.view.activity
 
+import android.app.ProgressDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.example.whoislcj.kotlin.model.bean.UserInfo
 import com.example.whoislcj.kotlin.presenter.impl.LoadDataPresenter
 import com.example.whoislcj.kotlin.view.interfaces.ILoadDataView
+import com.example.whoislcj.kotlin.view.widget.DelayedProgressDialog
 import com.example.whoislcj.testproject.R
 
-class MvpDemoActivity : AppCompatActivity() ,ILoadDataView<UserInfo> {
-    lateinit var userInfoTv:TextView;
+class MvpDemoActivity : AppCompatActivity(), ILoadDataView<UserInfo> {
+    lateinit var userInfoTv: TextView;
     lateinit var loadDataPresenter: LoadDataPresenter;
+    var progressDialog: DelayedProgressDialog? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mvp_demo)
-        loadDataPresenter= LoadDataPresenter(this);
+        loadDataPresenter = LoadDataPresenter(this);
 
-        userInfoTv =findViewById(R.id.text_user_info);
+        userInfoTv = findViewById(R.id.text_user_info);
 
         userInfoTv.setOnClickListener {
+
             loadDataPresenter.loadData();
+
         }
     }
 
     override fun startLoading() {
+        progressDialog?.dismiss();
+        progressDialog = DelayedProgressDialog();
+        progressDialog!!.show(getSupportFragmentManager(), "正在加载...");
     }
 
     override fun finishLoading() {
+        progressDialog?.dismiss();
     }
 
+
     override fun loadSuccess(result: UserInfo) {
-        userInfoTv.text=String.format("name:%s age:%d",result.name,result.age);
+        userInfoTv.text = String.format("name:%s age:%d", result.name, result.age);
     }
 
     override fun loadFailed() {
