1c3523f65ede16f69c24fce862d47d651dd0b330
whoislcj <102160369@qq.com>
2018-03-26 19:58:38 +0800

    kotlin mvp 完整使用

--- app/src/main/java/com/example/whoislcj/kotlin/model/impl/UserInfoModelImpl.kt
+++ app/src/main/java/com/example/whoislcj/kotlin/model/impl/UserInfoModelImpl.kt
@@ -1,9 +1,9 @@
 package com.example.whoislcj.kotlin.model.impl
 
+import android.os.Handler
 import com.example.whoislcj.kotlin.model.bean.UserInfo
 import com.example.whoislcj.kotlin.model.interfaces.GetUserInfoListener
 import com.example.whoislcj.kotlin.model.interfaces.IUserInfoModel
-import java.util.logging.Handler
 
 /**
  * Created by whoislcj on 2018/3/20.
@@ -12,12 +12,12 @@ class UserInfoModelImpl : IUserInfoModel {
 
 
     override fun getUserInfo(listener: GetUserInfoListener) {
-        android.os.Handler().postDelayed({
+        Handler().postDelayed({
             val userInfo = UserInfo();
             userInfo.age = 28;
             userInfo.name = "lcj";
             listener.onGetUserInfo(userInfo);
-        },5000)
+        },3000)
     }
 
 
